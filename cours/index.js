5f0e1ca7f5d1d1f4910530c94daf00f2f2175c8c
Ampy <rafiringa.a@gmail.com>
2023-06-21 14:47:32 +0300

    ajout cours + meal-app

--- cours/index.js
+++ cours/index.js
@@ -53,34 +53,85 @@ const init2 = {
   credentials: "same-origin",
 };
 
-document.querySelector("form").addEventListener("submit", () => {
-  fetch("http://localhost:3000/users", init2).then(() =>
-    console.log("data envoye")
-  );
-});
+// document.querySelector("form").addEventListener("submit", () => {
+//   fetch("http://localhost:3000/users", init2).then(() =>
+//     // console.log("data envoye")
+//   );
+// });
 
 //-------------
 // Asynchrone
 //-------------
 
-setTimeout(() => {
-  // console.log("test");
-}, 1000);
+// setTimeout(() => {
+//   // console.log("test");
+// }, 1000);
 
 // Promise
-fetch("monlien").then((res) => res);
+// fetch("monlien").then((res) => res);
 
 // async/await
-async function fetchData() {
-  await fetch("monlien");
-  // attend que le await soit execute avant de faire la suite
+// async function fetchData() {
+//   await fetch("monlien");
+// attend que le await soit execute avant de faire la suite
 
-  executeFonction();
-}
+//   executeFonction();
+// }
 
-const fetchData2 = async () => {
-  await fetch("monlien");
-  // attend que le await soit execute avant de faire la suite
+// const fetchData2 = async () => {
+//   await fetch("monlien");
+//   // attend que le await soit execute avant de faire la suite
 
-  executeFonction();
+//   executeFonction();
+// };
+
+// ---------
+// LE JSON
+// ---------
+
+// Methode .json() => methode qui s'auto-resout en envoyant le Body de la requete
+
+fetch("data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    // Stringify => convertit en JSON
+    let settings = JSON.stringify(data);
+    // Parse => transforme json en objet js
+    // console.log(JSON.parse(settings));
+  });
+
+// -------
+// WEB API
+// -------
+
+// CLIENT STORAGE
+//---------------
+
+// Local Storage
+localStorage.data = "Je stock la data";
+// document.body.textContent = localStorage.data;
+
+localStorage.removeItem("data");
+
+const obj = {
+  name: "Denis",
+  age: 22,
 };
+
+// Il faut passer des chaines de caracteres
+localStorage.user = JSON.stringify(obj);
+// console.log(JSON.parse(localStorage.user));
+
+// Session Storage
+sessionStorage.dataSettings = "55px";
+// console.log(sessionStorage.dataSettings);
+sessionStorage.clear();
+
+//--------
+// Cookie
+//--------
+document.cookie = "username=Rafiringa";
+
+// Bonne pratique
+document.cookie =
+  "pseudo=Rafh;path=/; max-age=450000; secure; samesite; expires=Thu, 31 Dec 2099 23:59:59 GMT";
